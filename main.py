9dc6b914f8db9acf9bd579b33a96265fcd4662d8
alecranjitkar <alexranjitkar@gmail.com>
2022-11-10 13:27:24 +0100

    added Timer

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main():
 
         if game.turn == WHITE:
             #value, new_board = minimax(game.get_board(), 4, WHITE, game) # The higher the depth the longer it will take to calculate
-            value, new_board = minimax_alpha_beta(game.get_board(), 4, WHITE, game, MIN_VALUE, MAX_VALUE)
+            value, new_board = minimax_alpha_beta(game.get_board(), 5, WHITE, game, MIN_VALUE, MAX_VALUE)
             game.ai_move(new_board)
 
         if game.winner() != None:
